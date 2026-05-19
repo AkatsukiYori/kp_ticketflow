61a3122e77658eb83f8e20df3eaf807f11b177cb
AkatsukiYori <alexander.then11@gmail.com>
2026-05-19 20:29:36 +0700

    Testing page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,11 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ticketflow</title>
 
-    <link rel="stylesheet" href="../../css/frontend.css">
+    <link rel="stylesheet" href="{{ asset('css/frontend.css') }}">
 </head>
 <body>
-    
+    <header>
+        <h3>hash</h3>
+    </header>
+    <main>
+        <h3>Ha/</h3>
+    </main>
 </body>
 </html>
