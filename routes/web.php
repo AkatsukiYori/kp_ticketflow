085107f0c34f99e36e1f364b3f427e9845a9a0e6
AkatsukiYori <alexander.then11@gmail.com>
2026-05-19 20:07:54 +0700

    Fix docker issue

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get("/new-ticket", function() {
-    return view("frontend/new_ticket");
+Route::get("/", function() {
+    return view("/frontend/index");
 });
