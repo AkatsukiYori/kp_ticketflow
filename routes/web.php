0489e18517bf0106e6c60b387f4f8c8f331ae064
AkatsukiYori <alexander.then11@gmail.com>
2026-05-19 19:41:53 +0700

    init file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/new-ticket", function() {
+    return view("frontend/new_ticket");
+});
