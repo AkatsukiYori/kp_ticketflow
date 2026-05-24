be52c9d1209bb02321444bc153fe00c56efbac51
AkatsukiYori <alexander.then11@gmail.com>
2026-05-24 15:07:27 +0700

    Design tampilan awal

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,15 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ticketflow</title>
+    <link rel="" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 
     <link rel="stylesheet" href="{{ asset('css/frontend.css') }}">
 </head>
 <body>
     <header>
-        <h3>hash</h3>
+        <div class="header">
+            <div>
+                <img src="{{ asset('assets/logo.webp') }}" alt="Logo">
+                <h1>TicketFlow</h1>
+            </div>
+                <button class="">Beranda</button>
+        </div>
     </header>
     <main>
-        <h3>Ha/</h3>
+        <div class="judul">
+            
+        </div>
+        <div class="gambar">
+            
+        </div>
     </main>
 </body>
 </html>
